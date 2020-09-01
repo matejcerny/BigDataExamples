bfee03b61157ae6d92dff5dbe31dfcac82bca798
cerny.matej <cerny.matej@gmail.com>
2020-09-01 15:56:16 +0200

    Frameless dependency

--- build.sbt
+++ build.sbt
@@ -1,32 +1,15 @@
+import Dependencies._
+
 Global / name := "BigDataExamples"
 Global / organization := "cz.matejcerny"
 Global / scalaVersion := "2.12.12"
 Global / assemblyJarName := s"${name.value}.jar"
 
-val sparkVersion = "3.0.0"
-val deltaVersion = "0.7.0"
-val hyperspaceVersion = "0.2.0"
-val slf4jVersion = "1.7.25"
-
 lazy val BigDataExamples = project
   .in(file("."))
   .settings(
-    libraryDependencies ++= dependencies
+    libraryDependencies ++= Delta ++ Frameless ++ Hyperspace ++ Logging ++ ScalaTest ++ Spark
   )
-
-lazy val dependencies = Seq(
-  // TEST
-  "org.scalatest" %% "scalatest" % "3.1.2" % Test,
-  "org.mockito" % "mockito-core" % "2.23.4" % Test,
-  // LOGS
-  "org.slf4j" % "slf4j-api" % slf4jVersion,
-  "org.slf4j" % "slf4j-log4j12" % slf4jVersion,
-  // SPARK
-  "org.apache.spark" %% "spark-core" % sparkVersion,
-  "org.apache.spark" %% "spark-sql" % sparkVersion,
-  "io.delta" %% "delta-core" % deltaVersion,
-  "com.microsoft.hyperspace" %% "hyperspace-core" % hyperspaceVersion
-)
 
 Global / scalacOptions ++= Seq(
   "-deprecation", // Emit warning and location for usages of deprecated APIs.
---
+++ project/Dependencies.scala
@@ -0,0 +1,46 @@
+import sbt._
+
+object Dependencies {
+
+  object Versions {
+    val Delta = "0.6.0"
+    val Frameless = "0.8.0"
+    val Hyperspace = "0.2.0"
+    val ScalaLogging = "3.9.2"
+    val ScalaTest = "3.1.2"
+    val SLF4J = "1.7.25"
+    val Spark = "2.4.4"
+  }
+
+  object Modules {
+    val Delta = "io.delta"
+    val Frameless = "org.typelevel"
+    val Hyperspace = "com.microsoft.hyperspace"
+    val ScalaLogging = "com.typesafe.scala-logging"
+    val ScalaTest = "org.scalatest"
+    val SLF4J = "org.slf4j"
+    val Spark = "org.apache.spark"
+  }
+
+  val Delta = Seq(Modules.Delta %% "delta-core" % Versions.Delta)
+
+  val Frameless = Seq(
+    Modules.Frameless %% "frameless-dataset" % Versions.Frameless,
+    Modules.Frameless %% "frameless-cats" % Versions.Frameless
+  )
+
+  val Hyperspace = Seq(Modules.Hyperspace %% "hyperspace-core" % Versions.Hyperspace)
+
+  val Logging = Seq(
+    Modules.ScalaLogging %% "scala-logging" % Versions.ScalaLogging,
+    Modules.SLF4J % "slf4j-api" % Versions.SLF4J
+  )
+
+  val ScalaTest = Seq(Modules.ScalaTest %% "scalatest" % Versions.ScalaTest % Test)
+
+  val Spark = Seq(
+    Modules.Spark %% "spark-core" % Versions.Spark,
+    Modules.Spark %% "spark-sql" % Versions.Spark
+  )
+
+}
