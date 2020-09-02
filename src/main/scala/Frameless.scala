ac14c2c2ce34738dc5c79a263e5662bcfd352404
cerny.matej <cerny.matej@gmail.com>
2020-09-02 17:33:02 +0200

    Frameless #2

--- src/main/scala/Frameless.scala
+++ src/main/scala/Frameless.scala
@@ -1,1 +1,16 @@
-object Frameless {}
+import frameless.TypedDataset
+
+object Frameless extends App with LocalSparkSession {
+
+  import sparkSession.implicits._
+  case class Obec(id: Long, nazev: String, okres: String)
+
+  val ds = Seq(
+    Obec(-997L, "AAA", "AAA"),
+    Obec(-998L, "BBB", "BBB"),
+    Obec(-999L, "CCC", "CCC")
+  ).toDS
+
+  val typedDS = TypedDataset.create(ds)
+
+}
