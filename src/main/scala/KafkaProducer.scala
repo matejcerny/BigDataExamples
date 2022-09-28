fa5a8eabbb95f9429485e1d9cf2c6b3af93ef6a6
Matěj Černý <matej.cerny@budgetbakers.com>
2022-09-28 10:45:41 +0200

    Kafka producer

--- src/main/scala/KafkaProducer.scala
+++ src/main/scala/KafkaProducer.scala
@@ -1,1 +1,28 @@
-object KafkaProducer extends App with LocalSparkSession {}
+import org.apache.spark.sql.types.{ StringType, StructField, StructType }
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+
+object KafkaProducer extends App with LocalSparkSession {
+
+  val schema = StructType(Seq(StructField("text", StringType)))
+  val duration = FiniteDuration(2, TimeUnit.SECONDS)
+
+  /** Streaming text files from disk */
+  val df = sparkSession.readStream
+    .schema(schema)
+    .text("data/stream")
+
+  val options = Map(
+    ("kafka.bootstrap.servers", "localhost:9092"),
+    ("topic", "topic1")
+  )
+
+  df
+    // .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+    .writeStream
+    .format("kafka")
+    .options(options)
+    .start()
+
+}
